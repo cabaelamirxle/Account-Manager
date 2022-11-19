e3dc905d98b3d5f0dc6330c23b190b84af06e8c9
cabaelamirxle <amirxlecabael28@gmail.com>
2022-11-19 21:38:22 +0800

    Final

--- accountManager.py
+++ accountManager.py
@@ -1,24 +1,23 @@
-pwd = input("What is the master password? ")
+master_pwd = input("What is the master password? ")
 
 def view():
     with open('accounts.txt', 'r') as f:
         for line in f.readlines():
-            data = (line.rstrip())
-            user, passw = data.split("|")
-            print("User: ", user, "| Password: ", passw)
+            print(line.rstrip())
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
-
+    
     with open('accounts.txt', 'a') as f:
         f.write(name + "|" + pwd + "\n")
+        
 
 while True:
-    mode = input("What you like, to add a new password or view existing ones(add, view), press q to quit ").lower()
+    mode = input("Would you like to add a new password or view existing ones(add, view)?, press q to quit. ")
     if mode == "q":
         break
-
+    
     if mode == "view":
         view()
     elif mode == "add":
