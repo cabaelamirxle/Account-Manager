790e7c7d697840fa00353fa9d3645005d237f219
cabaelamirxle <amirxlecabael28@gmail.com>
2022-11-19 19:50:17 +0800

    breaking user and password

--- accountManager.py
+++ accountManager.py
@@ -1,17 +1,19 @@
 pwd = input("What is the master password? ")
 
 def view():
-    pass
+    with open('accounts.txt', 'r') as f:
+        for line in f.readlines():
+            print(line)
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
 
     with open('accounts.txt', 'a') as f:
-        
+        f.write(name + "|" + pwd + "\n")
 
 while True:
-    mode = input("What you like, to add a new password or view existing ones(add, view), press q to quit").lower()
+    mode = input("What you like, to add a new password or view existing ones(add, view), press q to quit ").lower()
     if mode == "q":
         break
 
